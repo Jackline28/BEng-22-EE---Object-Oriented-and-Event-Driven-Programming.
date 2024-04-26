0dc3aa38299a97d41e6058632107c5bbbdacdf52
Jackline28 <167976765+Jackline28@users.noreply.github.com>
2024-04-26 13:24:09 +0300

    Create Female Class.java

---
+++ Female Class.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Jackline Bonifas
+ */
+public class Female {
+    void display() {
+        System.out.print("female properties :\n");
+        System.out.print("have breast :\n");
+        System.out.print("have long hair :\n");
+        System.out.print("have soft voice :\n");
+    }
+    
+}
