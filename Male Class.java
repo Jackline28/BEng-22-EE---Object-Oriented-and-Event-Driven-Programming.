ba03024d8f3c9f72630f0c1b657e0b3d2cea8c23
Jackline28 <167976765+Jackline28@users.noreply.github.com>
2024-04-26 13:25:06 +0300

    Create Male Class.java

---
+++ Male Class.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Jackline Bonifas
+ */
+public class Male {
+    void display(){
+    System.out.print("male properties :\n");
+    System.out.print("have ndevu :\n");
+    System.out.print("have short hair :\n");
+    
+            }
+    
+}
